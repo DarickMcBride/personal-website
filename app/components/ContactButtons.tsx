aae1e4c6594eaa2be18148cc0e985cd4d350b55f
Darick <darickmcbride@me.com>
2024-02-13 11:21:02 -0800

    added resume and fixed buttons

--- app/components/ContactButtons.tsx
+++ app/components/ContactButtons.tsx
@@ -1,32 +1,47 @@
-import { FaEnvelope, FaLinkedin, FaGithub } from "react-icons/fa";
+import { FaEnvelope, FaLinkedin, FaGithub, FaFilePdf } from "react-icons/fa";
+import Link from "next/link";
 
 export default function ContactButtons() {
   return (
-    <div className="flex space-x-4 justify-center text-primary pt-4 pb-4">
+    <div className="flex justify-center text-primary pt-4 pb-4 ">
       <a
-        className="btn btn-circle btn-ghost"
+        className="btn btn-ghost flex flex-col items-center justify-center "
         href="mailto:contactme@darickmcbride.com"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <FaEnvelope size={32} />
+        <FaEnvelope size={24} />
+        Email
       </a>
+
       <a
-        className="btn btn-circle btn-ghost"
+        className="btn btn-ghost flex flex-col items-center justify-center "
         href="https://www.linkedin.com/in/darickmcbride"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <FaLinkedin size={32} />
+        <FaLinkedin size={24} />
+        LinkedIn
       </a>
       <a
-        className="btn btn-circle btn-ghost"
+        className="btn btn-ghost flex flex-col items-center justify-center "
         href="https://github.com/DarickMcBride"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <FaGithub size={32} />
+        <FaGithub size={24} />
+        GitHub
       </a>
+      <Link
+        className="btn btn-ghost flex flex-col items-center justify-center "
+        href={"/resume.pdf"}
+        locale={false}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaFilePdf size={24} />
+        Resume
+      </Link>
     </div>
   );
 }
