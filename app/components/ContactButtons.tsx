b50a436685a521d2abb7adcadb6b81676050cefb
Darick McBride <darickmcbride@me.com>
2024-02-18 15:48:01 -0800

    made contact buttons larger

--- app/components/ContactButtons.tsx
+++ app/components/ContactButtons.tsx
@@ -5,7 +5,7 @@ export default function ContactButtons() {
   return (
     <div className="flex justify-center text-primary pt-4 pb-4 ">
       <a
-        className="btn btn-ghost flex flex-col items-center justify-center"
+        className="btn btn-ghost btn-lg flex flex-col items-center justify-center"
         href="mailto:contactme@darickmcbride.com"
         target="_blank"
         rel="noopener noreferrer"
@@ -15,7 +15,7 @@ export default function ContactButtons() {
       </a>
 
       <a
-        className="btn btn-ghost flex flex-col items-center justify-center "
+        className="btn btn-ghost btn-lg flex flex-col items-center justify-center "
         href="https://www.linkedin.com/in/darickmcbride"
         target="_blank"
         rel="noopener noreferrer"
@@ -24,7 +24,7 @@ export default function ContactButtons() {
         LinkedIn
       </a>
       <a
-        className="btn btn-ghost flex flex-col items-center justify-center "
+        className="btn btn-ghost btn-lg flex flex-col items-center justify-center "
         href="https://github.com/DarickMcBride"
         target="_blank"
         rel="noopener noreferrer"
@@ -33,7 +33,7 @@ export default function ContactButtons() {
         GitHub
       </a>
       <Link
-        className="btn btn-ghost flex flex-col items-center justify-center "
+        className="btn btn-ghost btn-lg flex flex-col items-center justify-center "
         href={
           "https://docs.google.com/document/d/1Y8AyyMWnrq9JYsG8c-L5VX3O5mKfpw19G8p5QI3vzcY/export?format=pdf"
         }
