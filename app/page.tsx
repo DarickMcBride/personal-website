0b5a898e0b08b4dc2ac5f9cfdcd14b37ee8d753f
Darick McBride <darickmcbride@me.com>
2024-02-18 21:01:46 -0800

    added swipeable cards

--- app/page.tsx
+++ app/page.tsx
@@ -13,62 +13,108 @@ export default function Home() {
           experience. Here&apos;s a closer look at the technologies I&apos;ve
           used to craft this site:
         </p>
-        <dl className="space-y-2">
-          <dt className="text-secondary font-semibold text-lg">React</dt>
-          <dd>
-            At the heart of my website is React, a popular JavaScript library
-            for building user interfaces. React allows me to create interactive
-            and responsive web applications that provide a smooth and enjoyable
-            browsing experience. It&apos;s the foundation of my website&apos;s
-            frontend, enabling me to build complex UI components and manage
-            state efficiently.
-          </dd>
-          <dt className="text-secondary font-semibold text-lg">
-            Next.js (App Router)
-          </dt>
-          <dd>
-            I&apos;ve leveraged Next.js, a powerful framework built on top of
-            React, to enhance the performance and SEO-friendliness of my
-            website. Next.js enables server-side rendering, automatic code
-            splitting, and simplified routing, all of which contribute to faster
-            load times and a better overall user experience.
-          </dd>
-          <dt className="text-secondary font-semibold text-semibold text-lg">
+        <div className="carousel ">
+          <div
+            id="item1"
+            className="carousel-item flex flex-col items-center w-full"
+          >
+            <div className="card w-96 glass">
+              <div className="card-body">
+                <h2 className="card-title text-secondary font-semibold">
+                  React
+                </h2>
+                <p>
+                  At the heart of my website is React, a popular JavaScript
+                  library for building user interfaces. React allows me to
+                  create interactive and responsive web applications that
+                  provide a smooth and enjoyable browsing experience. It&apos;s
+                  the foundation of my website&apos;s frontend, enabling me to
+                  build complex UI components and manage state efficiently.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div
+            id="item2"
+            className="carousel-item flex flex-col items-center w-full"
+          >
+            <h2 className="text-secondary font-semibold text-lg">
+              Next.js (App Router)
+            </h2>
+            <p>
+              I&apos;ve leveraged Next.js, a powerful framework built on top of
+              React, to enhance the performance and SEO-friendliness of my
+              website. Next.js enables server-side rendering, automatic code
+              splitting, and simplified routing, all of which contribute to
+              faster load times and a better overall user experience.
+            </p>
+          </div>
+          <div
+            id="item3"
+            className="carousel-item flex flex-col items-center w-full"
+          >
+            <h2 className="text-secondary font-semibold text-lg">
+              Tailwind CSS and daisyUI
+            </h2>
+            <p>
+              For styling my website, I&apos;ve chosen Tailwind CSS and daisyUI.
+              Tailwind CSS is a utility-first CSS framework that allows for
+              highly customizable designs with a low-level, component-friendly
+              approach. It promotes consistency, productivity and
+              maintainability across the project. daisyUI is a plugin for
+              Tailwind CSS that provides beautiful UI components, making it
+              easier to build a consistent and visually appealing interface.
+              Together, they offer a powerful combination for creating unique,
+              responsive, and user-friendly designs.
+            </p>
+          </div>
+          <div
+            id="item4"
+            className="carousel-item flex flex-col items-center w-full"
+          >
+            <h2 className="text-secondary font-semibold text-lg">TypeScript</h2>
+            <p>
+              I take code quality seriously, and that&apos;s why I&apos;ve
+              implemented TypeScript in my development process. TypeScript is a
+              statically typed superset of JavaScript, which helps catch errors
+              during development and enhances code maintainability and
+              scalability. It ensures that my codebase remains clean, readable,
+              and reliable.
+            </p>
+          </div>
+          <div
+            id="item5"
+            className="carousel-item flex flex-col items-center w-full"
+          >
+            <h2 className="text-secondary font-semibold text-lg">
+              GitHub Copilot
+            </h2>
+            <p>
+              I&apos;ve integrated GitHub Copilot into my development workflow
+              to streamline and enhance my code. Copilot provides intelligent
+              code suggestions and automation, making coding more efficient and
+              helping me deliver high-quality code faster.
+            </p>
+          </div>
+        </div>
+        <div className="flex justify-center w-full py-2 gap-2">
+          <a href="#item1" className="btn btn-xs">
+            React
+          </a>
+          <a href="#item2" className="btn btn-xs">
+            Next.js
+          </a>
+          <a href="#item3" className="btn btn-xs">
             Tailwind CSS and daisyUI
-          </dt>
-          <dd>
-            For styling my website, I&apos;ve chosen Tailwind CSS and daisyUI.
-            Tailwind CSS is a utility-first CSS framework that allows for highly
-            customizable designs with a low-level, component-friendly approach.
-            It promotes consistency, productivity and maintainability across the
-            project. daisyUI is a plugin for Tailwind CSS that provides
-            beautiful UI components, making it easier to build a consistent and
-            visually appealing interface. Together, they offer a powerful
-            combination for creating unique, responsive, and user-friendly
-            designs.
-          </dd>
-          <dt className="text-secondary font-semibold text-semibold text-lg">
+          </a>
+          <a href="#item4" className="btn btn-xs">
             TypeScript
-          </dt>
-          <dd>
-            I take code quality seriously, and that&apos;s why I&apos;ve
-            implemented TypeScript in my development process. TypeScript is a
-            statically typed superset of JavaScript, which helps catch errors
-            during development and enhances code maintainability and
-            scalability. It ensures that my codebase remains clean, readable,
-            and reliable.
-          </dd>
-          <dt className="text-secondary font-semibold text-lg">
+          </a>
+          <a href="#item5" className="btn btn-xs">
             GitHub Copilot
-          </dt>
-          <dd>
-            I&apos;ve integrated GitHub Copilot into my development workflow to
-            streamline and enhance my code. Copilot provides intelligent code
-            suggestions and automation, making coding more efficient and helping
-            me deliver high-quality code faster.
-          </dd>
-        </dl>
-        <div className="divider divider-secondary mx-[20%] py-4"></div>
+          </a>
+        </div>
+
         <p>
           My technology stack represents my commitment to delivering a
           high-quality, performant, and visually pleasing website. By using
