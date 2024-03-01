749e005d1e2dc742e88cf77303697e7edd6dc2f8
Darick McBride <darickmcbride@me.com>
2024-02-29 21:57:03 -0800

    typo

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,7 +4,7 @@ export default function NotFound() {
   return (
     <div>
       <h1>404</h1>
-      <p>Oops! We couldn't find this page.</p>
+      <p>Oops! We couldn&apos;t find this page.</p>
     </div>
   );
 }
