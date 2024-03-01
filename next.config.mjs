7db80135bd0aff7954246ac1b0d87ce5d6da563c
Darick McBride <darickmcbride@me.com>
2024-02-29 22:31:14 -0800

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  basePath: "/personal-website",
-  images: {
-    unoptimized: true,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
