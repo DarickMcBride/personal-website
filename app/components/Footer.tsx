d2d5c3c9366cefb9fe54ed3460fdaa4d4f0f07b3
Darick McBride <darickmcbride@me.com>
2024-05-05 22:06:22 -0700

    fixed typo

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <p>
-      This website was by built me with{" "}
+      This website was built by me with{" "}
       <span className="text-slate-200">Next.js</span> and deployed with{" "}
       <span className="text-slate-200">Vercel</span>. It uses{" "}
       <span className="text-slate-200">Tailwind CSS</span> and{" "}
