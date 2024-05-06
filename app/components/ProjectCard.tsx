de1537303aecb6bf0d9a240cedb7b7d7cffc233e
Darick McBride <darickmcbride@me.com>
2024-05-05 22:05:22 -0700

    fixed links

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -12,14 +12,16 @@ interface Project {
 
 export default function ProjectCard({ project }: { project: Project }) {
   return (
-    <div className="card card-side p-4 bg-none hover:bg-gray-700 hover:bg-opacity-40 hover:shadow-lg">
-      <figure>
-        <Image src={project.imageUrl} alt={project.title} className="w-36" />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title text-primary">{project.title}</h2>
-        <p>{project.description}</p>
+    <a href={project.projectUrl}>
+      <div className="card card-side p-4 bg-none hover:bg-gray-700 hover:bg-opacity-40 hover:shadow-lg">
+        <figure>
+          <Image src={project.imageUrl} alt={project.title} className="w-36" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title text-primary">{project.title}</h2>
+          <p>{project.description}</p>
+        </div>
       </div>
-    </div>
+    </a>
   );
 }
